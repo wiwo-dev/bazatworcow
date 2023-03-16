b04e8c2b967c653269ec3dee99036ee187fd0aa1
wiwo-one <wojtekwieclawski@gmail.com>
2023-03-16 18:41:07 +0530

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import React, { Requireable } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import PropTypes from 'prop-types';
-import reactToWebComponent from 'react-to-webcomponent';
-import Checklist from './components/checklist/Checklist';
-import { isChecklistItemType } from './types/checklistItem';
+import React, { Requireable } from "react";
+import * as ReactDOM from "react-dom/client";
+import PropTypes from "prop-types";
+import reactToWebComponent from "react-to-webcomponent";
+import Checklist from "./components/checklist/Checklist";
+import { isChecklistItemType } from "./types/checklistItem";
 
 class WebComponentWrapper extends React.Component<{ items: string }, {}> {
   static propTypes: {
@@ -18,11 +18,11 @@ class WebComponentWrapper extends React.Component<{ items: string }, {}> {
         items = undefined;
       } else if (items.length === 0) {
         items = undefined;
-      } else if (items?.some(item => !isChecklistItemType(item))) {
+      } else if (items?.some((item) => !isChecklistItemType(item))) {
         items = undefined;
       }
     } catch {
-      items = undefined
+      items = undefined;
     }
 
     return <Checklist items={items} />;
@@ -31,14 +31,17 @@ class WebComponentWrapper extends React.Component<{ items: string }, {}> {
 
 WebComponentWrapper.propTypes = {
   items: PropTypes.string,
-}
+};
 
 const wcChecklist = reactToWebComponent(WebComponentWrapper, React, ReactDOM, { dashStyleAttributes: true });
-const wcChecklistShadow = reactToWebComponent(WebComponentWrapper, React, ReactDOM, { dashStyleAttributes: true, shadow: true });
+const wcChecklistShadow = reactToWebComponent(WebComponentWrapper, React, ReactDOM, {
+  dashStyleAttributes: true,
+  shadow: true,
+});
 
 customElements.define("r2wc-checklist", wcChecklist);
 customElements.define("r2wc-checklist-shadow", wcChecklistShadow);
 
 // How to use:
-// <script defer="defer" src="https://rjspencer.github.io/r2wc-checklist/static/js/main.js"></script> 
+// <script defer="defer" src="https://rjspencer.github.io/r2wc-checklist/static/js/main.js"></script>
 // <r2wc-checklist items='[{"label":"First Thing","isChecked":false}]' />
